b1841819a91145ea7ead9f85faeac7a7ec13d2e6
Konstantin Savin <ks5470219@gmail.com>
2023-10-26 12:46:00 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+
+<body>
+   <form action="">
+      <div class="form-group">
+         <input type="text" name="" id="">
+      </div>
+      <div class="form-group">
+         <input type="text" name="" id="">
+      </div>
+      <div class="form-group">
+         <input type="text" name="" id="">
+      </div>
+      <div class="form-group">
+         <input type="text" name="" id="">
+      </div>
+      <div class="form-group">
+         <textarea name="" id="" cols="30" rows="10"></textarea>
+      </div>
+      <button type="submit"></button>
+   </form>
+</body>
+
+</html>
